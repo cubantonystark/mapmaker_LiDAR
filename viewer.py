086c0f6398320e43a7781c8e81ee4cc74085b5e5
Rey Rodriguez <escambray@outlook.com>
2024-08-16 20:59:10 -0400

    Delete viewer.py

--- viewer.py
+++
@@ -1,5 +0,0 @@
-from pyntcloud import PyntCloud
-
-human_face = PyntCloud.from_file("D:/Projects/MapMaker_Lidar/build/v1.0.5/ARTAK_MM/DATA/Generated_PointClouds/exyn_mine_interior.ply")
-
-human_face.plot()
